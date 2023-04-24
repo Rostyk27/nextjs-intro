8f9b1bea38ef16d21ac31f09a7c942867ef9b021
Rostyk Chaikivskyi <r.chaikivskyi@gmail.com>
2023-04-24 18:20:44 +0300

    home styles

---
+++ app/contact/page.tsx
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+
+export default async function Contact() {
+  return (
+    <div className="container">
+      <h1>Contact page</h1>
+
+      <Link href="/">Home</Link>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,26 @@
+import Link from 'next/link';
+import styles from '../styles/home.module.scss';
+
 export default async function Home() {
-  return <div>hello</div>;
+  return (
+    <div className={styles.screen}>
+      <div>
+        <div>
+          <h1 className={styles.title}>Hi, my name is Rostyk!</h1>
+        </div>
+
+        <div>
+          <Link href="/blog" className={styles.link}>
+            Checkout my Blog
+          </Link>
+        </div>
+
+        <div>
+          <Link href="/contact" className={styles.link}>
+            Contact me
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
